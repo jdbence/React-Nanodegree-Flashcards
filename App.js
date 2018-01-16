11190e0e5facaabcf9e82b33a18827d62637dc82
Joshua Bence <joshua.bence@gmail.com>
2018-01-15 23:01:17 -0800

    chore: added basic 2 pages

--- App.js
+++ App.js
@@ -1,14 +1,46 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TouchableOpacity, View, FlatList, StatusBar } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+const Quiz = ({ navigation }) => (
+  <View>
+    <Text>This is the Quiz view</Text>
+  </View>
+);
+const list = Array.apply(null, Array(50)).map(function (x, i) { return {key: i}; })
+
+const Home = ({ navigation }) => (
+  <View style={styles.container}>
+    <FlatList
+      data={list}
+      renderItem={({item}) =>
+      <TouchableOpacity onPress={() => navigation.navigate('Quiz')}>
+        <Text style={styles.item}>{item.key}</Text>
+      </TouchableOpacity>
+    }
+    />
+  </View>
+);
+const log = function () {
+  console.log('new quiz')
+}
+const Stack = StackNavigator({
+  Home: {
+    screen: Home,
+    navigationOptions: ({navigation}) => ({
+      title: 'Quizzes',
+      headerRight: <Button color={'green'} title={'New Quiz'} onPress={log}/>,
+    }),
+  },
+  Quiz: {
+    screen: Quiz
+  }
+})
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Stack />
+
     );
   }
 }
@@ -17,7 +49,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+    borderBottomColor: 'black',
+    borderBottomWidth: 1
   },
 });
